502f415e4dee27300b489ecf160a061042e08411
DEADBYTE <termay10000@gmail.com>
2026-06-17 07:12:49 +0700

    Allow RichEdit SDK nameless structs

--- build.bat
+++ build.bat
@@ -21,7 +21,7 @@ exit /b 1
 :build
 if not exist build mkdir build
 
-cl /nologo /W4 /WX /O1 /DUNICODE /D_UNICODE src\dbytepad.c ^
+cl /nologo /W4 /WX /wd4201 /O1 /DUNICODE /D_UNICODE src\dbytepad.c ^
   /link /SUBSYSTEM:WINDOWS /OUT:build\dbytepad.exe user32.lib gdi32.lib comdlg32.lib shell32.lib comctl32.lib
 
 if errorlevel 1 exit /b 1
